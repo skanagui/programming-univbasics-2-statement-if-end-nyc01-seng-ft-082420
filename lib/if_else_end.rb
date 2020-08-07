b1ea2baa8a5c6f8d3a67452cd037bf7e60b681d4
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-07 15:25:13 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -16,3 +16,12 @@ else
   puts "Enjoy the fine day!"
 end
 puts "Oh, and always wear sunscreen!"
+
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
